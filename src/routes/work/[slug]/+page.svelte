781dda92b3a05e498834c1341164a800291b8117
BenoitRobin <benoitrobin@me.com>
2025-01-21 12:07:08 +0100

    feat: :construction: Render blog article on frontend
    Render blog article on frontend

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,4 +1,48 @@
 <script lang="ts">
-	const { data } = $props();
+	let { data } = $props();
+
+    const { company, name, dateAccomplished, stack, projectImageUrl, slug, content } = data.project;
+
+    function getTagFromStyle(style: ProcessedTextContent['style']): string {
+        if (style === "normal") {
+            return "p";
+        } else {
+            return style;
+        }
+    }
+
+
 	$inspect(data);
 </script>
+
+
+<main class="default-margin work-page">
+<h4>{company}</h4>
+<div class="underscore"></div>
+<h2 class="mb-s">{name}</h2>
+<img src={projectImageUrl} alt={name} class="project-image">
+<div class="project-container mt-m">
+    <div class="meta-data">
+        <h3 class="semi-bold">Date</h3>
+        <p>{dateAccomplished.slice(0, 7)}</p>
+        <h3 class="semi-bold mt-m">Tech Stack</h3>
+        <ul>
+            {#each stack as skill}
+            <li>{skill}</li>
+            {/each}
+        </ul>
+    </div>
+    <div class="project-text">
+        {#each content as block}
+        {#if block.type === 'text'}
+            <svelte:element this={getTagFromStyle(block.style)}>
+                {block.textToRender}
+            </svelte:element>
+            {:else}
+            <img src={block.url} alt="" class="content-image">
+        {/if}
+            
+        {/each}
+    </div>
+</div>
+</main>
