8e126519cf2c3104505638839bac88b494a0a12d
BenoitRobin <benoitrobin@me.com>
2025-01-21 12:17:39 +0100

    feat: :sparkles: blog article styling
    blog article styling

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,48 +1,77 @@
 <script lang="ts">
 	let { data } = $props();
 
-    const { company, name, dateAccomplished, stack, projectImageUrl, slug, content } = data.project;
+	const { company, name, dateAccomplished, stack, projectImageUrl, slug, content } = data.project;
 
-    function getTagFromStyle(style: ProcessedTextContent['style']): string {
-        if (style === "normal") {
-            return "p";
-        } else {
-            return style;
-        }
-    }
-
+	function getTagFromStyle(style: ProcessedTextContent['style']): string {
+		if (style === 'normal') {
+			return 'p';
+		} else {
+			return style;
+		}
+	}
 
 	$inspect(data);
 </script>
 
-
 <main class="default-margin work-page">
-<h4>{company}</h4>
-<div class="underscore"></div>
-<h2 class="mb-s">{name}</h2>
-<img src={projectImageUrl} alt={name} class="project-image">
-<div class="project-container mt-m">
-    <div class="meta-data">
-        <h3 class="semi-bold">Date</h3>
-        <p>{dateAccomplished.slice(0, 7)}</p>
-        <h3 class="semi-bold mt-m">Tech Stack</h3>
-        <ul>
-            {#each stack as skill}
-            <li>{skill}</li>
-            {/each}
-        </ul>
-    </div>
-    <div class="project-text">
-        {#each content as block}
-        {#if block.type === 'text'}
-            <svelte:element this={getTagFromStyle(block.style)}>
-                {block.textToRender}
-            </svelte:element>
-            {:else}
-            <img src={block.url} alt="" class="content-image">
-        {/if}
-            
-        {/each}
-    </div>
-</div>
+	<h4>{company}</h4>
+	<div class="underscore"></div>
+	<h2 class="mb-s">{name}</h2>
+	<img src={projectImageUrl} alt={name} class="project-image" />
+	<div class="project-container mt-m">
+		<div class="meta-data">
+			<h3 class="semi-bold">Date</h3>
+            <div class="underscore"></div>
+			<p>{dateAccomplished.slice(0, 7)}</p>
+			<h3 class="semi-bold mt-m">Tech Stack</h3>
+            <div class="underscore"></div>
+			<ul>
+				{#each stack as skill}
+					<li>{skill}</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="project-text">
+			{#each content as block}
+				{#if block.type === 'text'}
+					<svelte:element this={getTagFromStyle(block.style)}>
+						{block.textToRender}
+					</svelte:element>
+				{:else}
+					<img src={block.url} alt="" class="content-image" />
+				{/if}
+			{/each}
+		</div>
+	</div>
 </main>
+
+<style>
+	.work-page {
+		padding-top: 80px;
+		padding-bottom: 140px;
+	}
+
+	.project-image {
+		width: 100%;
+		max-height: 450px;
+		object-fit: cover;
+		border-radius: 20px;
+        box-shadow: 5px 5px 8px -2px #000000;
+	}
+
+	.content-image {
+		width: 100%;
+		border-radius: 20px;
+		box-shadow: 5px 5px 8px -2px #000000;
+        margin:40px 0;
+	}
+
+	.project-container {
+		display: flex;
+	}
+
+	.meta-data {
+		min-width: 200px;
+	}
+</style>
